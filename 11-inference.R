44120ecd7f7713297ba7c26f67a6a166c8a0258a
Oscar Roa <oscaroa196@gmail.com>
2019-10-08 20:13:54 -0500

    Refactor script.
    Change points behavior, modify transparency and color for each posterior distribution up to 9 observations.

--- 11-inference.R
+++ 11-inference.R
@@ -9,21 +9,7 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 source("distributions/pmf.R") # Funciones de masa de probabilidad
 
 # Parametro desconocido de la distribucion hipergeometrica
-m <- 0:20
-# Carga probabilistica a priori inicial
-# P_m <- c(rep(0, 19), 7, 6)
-
-# Cargada a la derecha
-P_m <- c(1,1,1,2,2,3,3,4,5,6,6,7,7,7,7,1,1,1,1,1,1) # Carga probabilistica a priori
-
-# Centrada
-# P_m <- c(1,1,1,2,2,6,7,7,7,6,6,5,3,1,1,1,1,1,1,1,1) # Carga probabilistica a priori
-
-# Izquierda
-# P_m <- c(2,2,2,3,3,6,6,7,4,4,3,2,1,1,1,1,1,1,1,1,1) # Carga probabilistica a priori
-
-# P_m <- 21:1
-# P_m <- 1:21
+m <- c(1,3,6,6,7,7,6,5,3,3,2,2,1,1,1,1,1,2,2,2,1)
 P_m <- P_m/sum(P_m) # Proporciones de las cargas probabilisticas a priori (distribucion a priori)
 
 # Parametros conocidos de la distribucion hipergeometrica
@@ -41,7 +27,6 @@ P_X_m <- array(dim = dim(P_x_d_m))
 P_x <- array(dim = length(x))
 
 # Iteramos sobre el posible número de éxitos en la poblacion (m)
-
 for (m_hyp in m) {
   # Ya que estamos trabajando con una distribucion hipergeometrica
   # tenemos que calcular la distribucion para cada posible valor y asignarlo
@@ -63,23 +48,14 @@ for (x_hyp in x) {
 x_obs <- 1
 pos <- which(x==x_obs)
 P_m_d_x <- P_X_m[, pos]/P_x[pos]
-
 P_m <- P_m_d_x # Posterior después de la primera observacion
-# hasta la prior antes de la segunda extracción
 
+# Matrices a llenar con la nueva distribucion de probabilidad P_m
 P_x_d_m <- array(dim = c(length(m), length(x)))
-# Matriz probabilidad conjunta P(m, x)
 P_X_m <- array(dim = dim(P_x_d_m))
-# Array de la distribucion de X
 P_x <- array(dim = length(x))
 
-# Iteramos sobre el posible número de éxitos en la poblacion (m)
-
 for (m_hyp in m) {
-  # Ya que estamos trabajando con una distribucion hipergeometrica
-  # tenemos que calcular la distribucion para cada posible valor y asignarlo
-  # a una posicion en la matriz de la probabilidad conjunta que corresponda a
-  # los valores de m
   pos = which(m == m_hyp)
   
   P_x_d_m[pos, ] <- hipergeom_dist(x, m_hyp, n, k)
@@ -95,23 +71,14 @@ for (x_hyp in x) {
 x_obs <- 4
 pos <- which(x==x_obs)
 P_m_d_x <- P_X_m[, pos]/P_x[pos]
+P_m <- P_m_d_x # Posterior después de la segunda observacion
 
-P_m <- P_m_d_x # Posterior después de la primera observacion
-# hasta la prior antes de la segunda extracción
-
+# Matrices a llenar con la nueva distribucion de propabilidad P_m
 P_x_d_m <- array(dim = c(length(m), length(x)))
-# Matriz probabilidad conjunta P(m, x)
 P_X_m <- array(dim = dim(P_x_d_m))
-# Array de la distribucion de X
 P_x <- array(dim = length(x))
 
-# Iteramos sobre el posible número de éxitos en la poblacion (m)
-
 for (m_hyp in m) {
-  # Ya que estamos trabajando con una distribucion hipergeometrica
-  # tenemos que calcular la distribucion para cada posible valor y asignarlo
-  # a una posicion en la matriz de la probabilidad conjunta que corresponda a
-  # los valores de m
   pos = which(m == m_hyp)
   
   P_x_d_m[pos, ] <- hipergeom_dist(x, m_hyp, n, k)
@@ -123,19 +90,15 @@ for (x_hyp in x) {
   P_x[pos] <- sum(P_X_m[, pos], na.rm = T)
 }
 
-# # Tercer dato observado
+# Tercer dato observado
 x_obs <- 4
 pos <- which(x==x_obs)
 P_m_d_x <- P_X_m[, pos]/P_x[pos]
-P_m <- P_m_d_x # Posterior después de la segunda observacion
+P_m <- P_m_d_x # Posterior después de la tercera observacion
 
 P_x_d_m <- array(dim = c(length(m), length(x)))
-# Matriz probabilidad conjunta P(m, x)
 P_X_m <- array(dim = dim(P_x_d_m))
-# Array de la distribucion de X
 P_x <- array(dim = length(x))
-
-# Iteramos sobre el posible número de éxitos en la poblacion (m)
 
 for (m_hyp in m) {
   # Ya que estamos trabajando con una distribucion hipergeometrica
@@ -152,8 +115,6 @@ for (x_hyp in x) {
   pos = which(x == x_hyp)
   P_x[pos] <- sum(P_X_m[, pos], na.rm = T)
 }
-
-# # hasta la prior antes de la segunda extracción
 
 plot(m, P_m, col = "#aaaaaa", lwd = 6, type = "h", ylim = c(0, .5))
 points(m, P_m_d_x, col = "#222222", lwd = 6, type = "h")
@@ -186,9 +147,8 @@ plot(x, P_x,
 mtext("Distribucion marginal de X", 3, line = 1)
 
 
-
-
-
+# Solucion iterativa
+# Valida para n observaciones
 
 rm(list = ls())
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
@@ -213,49 +173,42 @@ x_observada <- c(1, 4, 4, 2, 3, 3, 4, 2, 3)
 
 
 for (indice in 1:(length(x_observada))) {
-  
-  
-  # Cargada a la derecha
+
+  # Condicional para asignar la carga inicial de la distribucion a priori
   if(indice == 1) {
-    P_m <- c(1,1,1,2,2,3,3,4,5,6,6,7,7,7,7,1,1,1,1,1,1) # Carga probabilistica a priori
-    
-    P_m <- P_m/sum(P_m) # Proporciones de las cargas probabilisticas a priori (distribucion a priori)
+    P_m <- c(1,3,6,6,7,7,6,5,3,3,2,2,1,1,1,1,1,2,2,2,1)
+    P_m <- P_m/sum(P_m)
     plot(m, P_m, type = 'b', pch=16, ylim=c(0, 0.5))
   }
+  # Condicional para las cargas posteriores con base en los datos observados
   else if (indice > 1) {
     P_m <- P_m_d_x
   }
-  
+
+  # Matrices a llenar por cada nueva distribucion a priori
   P_x_d_m <- array(dim = c(length(m), length(x)))
-  # Matriz probabilidad conjunta P(m, x)
   P_X_m <- array(dim = dim(P_x_d_m))
-  # Array de la distribucion de X
   P_x <- array(dim = length(x))
-  
-  # Iteramos sobre el posible número de éxitos en la poblacion (m)
-  
+
   for (m_hyp in m) {
-    # Ya que estamos trabajando con una distribucion hipergeometrica
-    # tenemos que calcular la distribucion para cada posible valor y asignarlo
-    # a una posicion en la matriz de la probabilidad conjunta que corresponda a
-    # los valores de m
     pos = which(m == m_hyp)
     
     P_x_d_m[pos, ] <- hipergeom_dist(x, m_hyp, n, k)
     P_X_m[pos, ] <- P_x_d_m[pos, ]*P_m[pos]
   }
-  
+
   for (x_hyp in x) {
     pos = which(x == x_hyp)
     P_x[pos] <- sum(P_X_m[, pos], na.rm = T)
   }
-  
-  
+
   # posterior
   x_obs <- x_observada[indice]
-  P_m_d_x <- P_X_m[, which(x==x_obs)]/P_x[which(x==x_obs)]
-  color <- paste("#", indice, indice, "ffff")
-  points(m, P_m_d_x, type='o', pch=16-indice, col = "red")
+  pos = which(x==x_obs)
+  P_m_d_x <- P_X_m[, pos]/P_x[pos]
+
+  color <- paste0("#", indice-1, "f0000", indice, "f")
+  points(m, P_m_d_x, type='o', pch=14+indice, col = color, bg = "gray")
 }
 
 points(m, P_m_d_x, cex=1.5, col='red')
