c56c0eec18b0b6beeed7ac05336db8551648cfd1
miusuario <miusuario@micorreo.com>
2019-11-07 14:40:49 -0600

    Add prior variables to the model.
    TODO: Find out what or how to do the predictions on K.

--- 19.1-inference-binomial-2unknown-jags.R
+++ 19.1-inference-binomial-2unknown-jags.R
@@ -1,12 +1,14 @@
 ###################################################
 #   TODO: Obtener y graficar expectativas a priori entre de theta, n
 #         y las predictivas
+#         DONE
+#   TODO: Descubrir lo que puede hacerse sobre las predicciones prior y post de K
 rm(list = ls())
 library("R2jags")
 
 k_observada <- 24
 
-variables_no_observables <- c('theta_post', 'n_post')
+variables_no_observables <- c('theta_post_pred', 'theta_prior_pred', 'n_post_pred', 'n_prior_pred')
 
 # Variables observables
 variables_observables <- list('k_observada')
@@ -14,11 +16,12 @@ variables_observables <- list('k_observada')
 # Model
 write("
   model {
-    theta_post ~dbeta(1,1)
+    theta_post_pred ~dbeta(1,1)
+    theta_prior_pred ~dbeta(1,1)
     vector <- rep(1/150, 150)
-    n_post ~dcat(vector)
-    
-    k_observada ~dbin(theta_post, n_post)
+    n_post_pred ~dcat(vector)
+    n_prior_pred ~dcat(vector)
+    k_observada ~dbin(theta_post_pred, n_post_pred)
   }
   ", "model_name.bug")
 
@@ -41,9 +44,13 @@ joint_color <- "#00ee0088"
 layout(1:2)
 par(mar = rep(2, 4))
 
-hist(nodes$theta_post, col = post_color, freq = F, xlim = c(0, 1), main = "Theta post")
-hist(nodes$n_post, col = post_color, freq = F, xlim = c(0, 150), main = "N post")
+hist(nodes$theta_post_pred, col = post_color, freq = F, xlim = c(0, 1), main = "Theta post")
+hist(nodes$theta_prior_pred, col = prior_color, freq = F, xlim = c(0, 1), main = "Theta prior")
+
+hist(nodes$n_post_pred, col = post_color, freq = F, xlim = c(0, 150), main = "N post")
+hist(nodes$n_prior_pred, col = prior_color, freq = F, xlim = c(0, 150), main = "N prior")
 
 layout(1)
-# Grafica de la distribucion conjunta entre los dos parametros no observables
-plot(nodes$n_post, nodes$theta_post, pch = 21, bg = joint_color)
+# Graficas de la distribucion conjunta entre los dos parametros no observables
+plot(nodes$n_post_pred, nodes$theta_post_pred, pch = 21, bg = joint_color, main = "P(n_post_pred, theta_post_pred)")
+plot(nodes$n_prior_pred, nodes$theta_prior_pred, pch = 21, bg = joint_color, main = "P(n_prior_pred, theta_prior_pred)")
